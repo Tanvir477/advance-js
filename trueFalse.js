df1354aa748aef7bf0678ec7784033e3917a5430
tanvir477 <tanvirhassan477@gmail.com>
2022-07-02 01:36:47 +0600

    true-false

---
+++ trueFalse.js
@@ -0,0 +1,39 @@
+const age=4;
+
+if(age){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
+//For string
+const  name=4;
+
+if(name.length > 0){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
+// if undefine
+let  name1; 
+console.log(name1);
+
+if(name1){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
+
+// false value =
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// Truthy
+// "0", " ", [] also all 
+
